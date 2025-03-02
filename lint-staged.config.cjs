85b041ab92ad409945161f12891b945a35b20710
Vector341 <1786816365@qq.com>
2025-03-02 19:11:52 +0800

    feat: add esmodule support

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -3,5 +3,5 @@ module.exports = {
     "prettier --cache --write --ignore-unknown",
     "cspell --cache --no-must-find-files",
   ],
-  "*.js": ["eslint --cache --fix"],
+  "*.[c|m]js": ["eslint --cache --fix"],
 };
